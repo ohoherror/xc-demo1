ed62962bb8fdac4250bb1f146a8b7db0e41a4642
ohoherror <1458251589@qq.com>
2019-01-17 20:42:24 +0800

    混乱

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="y-nav">
     <div class="y_nav_h">
-      <img src="../assets/images/homepage_man.png" alt>年轻哪能不放纵，high起来
+      <img src="../assets/images/homepage_man.png" alt>年轻哪能不放纵，约起来
     </div>
     <ul>
       <li>
@@ -13,13 +13,13 @@
       <li>
         <router-link to="/user/navbar">
           <i class="icon">&#xe666;</i>
-          <span>我的跑步</span>
+          <span>我的约跑</span>
         </router-link>
       </li>
       <li>
         <router-link to="/user/navbar">
           <i class="icon">&#xe6bd;</i>
-          <span>我的步行</span>
+          <span>我的约行</span>
         </router-link>
       </li>
       <li>
@@ -35,13 +35,13 @@
 <script>
 export default {
   data() {
-    return {};
+    return {}
   }
-};
+}
 </script>
 
-<style lang="scss" scoped>
-@import "../assets/css/function";
+<style scoped lang="scss">
+@import '../assets/css/function';
 
 .y-nav {
   background: #fff;
@@ -49,6 +49,39 @@ export default {
   .y_nav_h {
     width: px2rem(380px);
     margin: 0 auto;
+    text-align: center;
+    padding-top: 5px;
+    position: relative;
+    padding-bottom: 3px;
+    border-bottom: 1px solid #000;
+    img {
+      width: px2rem(60px);
+      height: px2rem(55px);
+      margin-left: -5px;
+      vertical-align: middle;
+    }
+  }
+  ul {
+    display: flex;
+    margin-top: px2rem(15px);
+    li {
+      width: 25%;
+      text-align: center;
+      .icon {
+        display: block;
+        width: px2rem(110px);
+        height: px2rem(110px);
+        line-height: px2rem(110px);
+        background: #2ad2c9;
+        font-size: 22px;
+        margin: 0 auto;
+        border-radius: 45%;
+        margin-bottom: px2rem(15px);
+      }
+      span {
+        font-size: 13px;
+      }
+    }
   }
 }
 </style>
---
+++ src/components/travel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="travel">
+    <router-link :to="{path:'/travel/'+travel.ObjectId}"></router-link>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    travel: Object
+  },
+  data() {
+    return {}
+  },
+  filters: {
+    formatTime(value) {
+      return moment(value).format(YYYY - MM - DD)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
